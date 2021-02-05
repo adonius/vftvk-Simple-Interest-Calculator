de5fcdf29dba23f0459e08b1f1bef434887623a8
adonious <adonisstathakis@gmail.com>
2021-02-05 15:05:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ function compute()
    var p = document.getElementById("principal").value;
    if(p<=0)
    {
-       alert("enter a positive number greater than zero")
-       document.getElementById("prinicipal").focus()
-       document.getElementById("prinicipal").click()
-       return null
+       alert("enter a positive number greater than zero");
+       document.getElementById("prinicipal").focus();
+       document.getElementById("prinicipal").click();
+       return null;
    }
    var r = document.getElementById("rate").value;
    var y =document.getElementById("years").value;
-   console.log(y)
-   var interest = p*r*y/100
-   console.log(p*r)
-   var ndate = new Date()
-   newdate = ndate.getFullYear()
-   newdate+= parseInt(y)
-   document.getElementById("result").innerHTML =  "<br>" + "<br>" + "If you deposit " + "<span class='highlight'>" + p + '</span>' +'<br>' + ' at an interest rate of ' + "<span class='highlight'>" + r + '</span>' +'%.' +'<br>' + 'You will recive an amount of ' + "<span class='highlight'>" +interest + '</span>' +'<br>' + 'In the year ' + "<span class='highlight'>" +newdate + '</span>'
+   console.log(y);
+   var interest = p*r*y/100;
+   console.log(p*r);
+   var ndate = new Date();
+   newdate = ndate.getFullYear();
+   newdate+= parseInt(y);
+   document.getElementById("result").innerHTML =  "<br>" + "<br>" + "If you deposit " + "<span class='highlight'>" + p + '</span>' +'<br>' + ' at an interest rate of ' + "<span class='highlight'>" + r + '</span>' +'%.' +'<br>' + 'You will recive an amount of ' + "<span class='highlight'>" +interest + '</span>' +'<br>' + 'In the year ' + "<span class='highlight'>" +newdate + '</span>';
 }
 function ratechange()
 {
